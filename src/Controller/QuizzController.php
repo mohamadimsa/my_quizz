d7ac476abd15c0150c75475739e244dbb64bda51
mohamed <mohamadi1.msa-abdou@epitech.eu>
2021-05-06 20:46:08 +0200

    affichage des champs reponse avect form builder

--- src/Controller/QuizzController.php
+++ src/Controller/QuizzController.php
@@ -7,11 +7,17 @@ use App\Entity\Quizz;
 use App\Repository\QuestionRepository;
 use App\Repository\QuizzRepository;
 use App\Repository\ReponseRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Flex\Path;
 
 class QuizzController extends AbstractController
 {
@@ -29,7 +35,7 @@ class QuizzController extends AbstractController
     /**
      * @Route("/quizz/{id_quizz}", name="quizz_show")
      */
-    public function showQuizz(Request $request, QuestionRepository $questionRepository, ReponseRepository $reponse, $id_quizz, PaginatorInterface $paginator,): Response
+    public function showQuizz(Request $request, QuestionRepository $questionRepository, ReponseRepository $reponse, $id_quizz, PaginatorInterface $paginator, EntityManagerInterface $em): Response
     {
         $donnees = $questionRepository->findBy([
             "quizz" => $id_quizz
@@ -43,14 +49,46 @@ class QuizzController extends AbstractController
         foreach ($questions as  $value) {
             $id_question = $value;
         }
-
         $donnees_reponse = $reponse->findBy(([
             "question" => $id_question
         ]));
+
+
         
+ $option = [];
+        for ($i=0; $i < count($donnees_reponse) ; $i++) { 
+
+            $reponsee = str_replace ( '.' , '~' , $donnees_reponse[$i]->getReponse());
+            
+            $option[$reponsee] = $reponsee;
+        }
+
+         $question_suivante = $request->query->getInt('question',1)+1;
+
+       $form = $this->createFormBuilder()
+               ->setAction($this->generateUrl('quizz_show',[
+                   "id_quizz" => $id_quizz,
+                   "question" => $question_suivante
+
+               ]))
+               ->setMethod('POST')
+               ->add("Selectionne_la_bonne_reponse_:",ChoiceType::class,[
+                   'choices'=> $option,
+                   'expanded'=> true
+               ])
+               ->add("Question_Suivante",SubmitType::class)
+               ->getForm()
+        ;
+
+       $view = $form->createView();
+
+
+
+
         return $this->render('quizz/quizz.html.twig', [
             'questions' => $questions,
-            "reponses" => $donnees_reponse
+            "reponses" => $donnees_reponse,
+            "monformulaire" => $view
         ]);
     }
 }
---
+++ src/Form/ReponseType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReponseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add("rodio",RadioType::class,[
+             "label" => "radio1",
+             "label" => "radio2",
+         ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
