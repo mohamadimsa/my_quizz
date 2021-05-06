d83200504a829f54cb18683bfacb53361eb8bbe3
mohamed <mohamadi1.msa-abdou@epitech.eu>
2021-05-06 14:59:25 +0200

    recuperation des reponse  associer au question ok

--- src/Controller/QuizzController.php
+++ src/Controller/QuizzController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\Entity\Question;
 use App\Entity\Quizz;
 use App\Repository\QuestionRepository;
+use App\Repository\QuizzRepository;
+use App\Repository\ReponseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Knp\Component\Pager\PaginatorInterface; 
+use Knp\Component\Pager\PaginatorInterface;
 
 class QuizzController extends AbstractController
 {
@@ -17,8 +19,8 @@ class QuizzController extends AbstractController
     public function index($id_categories): Response
     {
         $quizzs = $this->getDoctrine()->getRepository(Quizz::class)->findBy(
-                ['categories' => $id_categories]
-            );
+            ['categories' => $id_categories]
+        );
         return $this->render('quizz/index.html.twig', [
             'quizzs' => $quizzs,
         ]);
@@ -27,20 +29,28 @@ class QuizzController extends AbstractController
     /**
      * @Route("/quizz/{id_quizz}", name="quizz_show")
      */
-    public function showQuizz(Request $request,QuestionRepository $questionRepository, $id_quizz,PaginatorInterface $paginator,):Response
+    public function showQuizz(Request $request, QuestionRepository $questionRepository, ReponseRepository $reponse, $id_quizz, PaginatorInterface $paginator,): Response
     {
-       $donnees = $questionRepository->findBy([
+        $donnees = $questionRepository->findBy([
             "quizz" => $id_quizz
         ]);
-        
         $questions = $paginator->paginate(
             $donnees,
             $request->query->getInt('question', 1),
             1 // Nombre de résultats par page
         );
 
+        foreach ($questions as  $value) {
+            $id_question = $value;
+        }
+
+        $donnees_reponse = $reponse->findBy(([
+            "question" => $id_question
+        ]));
+        
         return $this->render('quizz/quizz.html.twig', [
             'questions' => $questions,
+            "reponses" => $donnees_reponse
         ]);
     }
 }
