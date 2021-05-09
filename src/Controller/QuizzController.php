b5f13b7d1869fcca326bb29a732cbbb70133576f
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-05-09 22:33:38 +0200

    creation du formulaire de creation de quizz

--- src/Controller/QuizzController.php
+++ src/Controller/QuizzController.php
@@ -9,17 +9,15 @@ use App\Repository\QuizzRepository;
 use App\Repository\ReponseRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\RedirectController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Flex\Path;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class QuizzController extends AbstractController
 {
@@ -35,7 +33,54 @@ class QuizzController extends AbstractController
     }
 
     /**
-     * @Route("/quizz/{id_quizz}", name="quizz_show")
+     * @Route("/create/quizz", "quizz_create")
+     */
+    public function create()
+    {
+        
+        $comp = 0;
+        $form = $this->createFormBuilder()
+            ->add("name_quizz", TextType::class,[
+                "label" => "Nom du quizz : "
+            ]);
+
+        for ($i = 1; $i < 11; $i++) {
+
+            $form->add("question_$i", TextType::class)
+             ->add("rep_1_$i",TextType::class,[
+                 "label"=> "choix :1"
+             ])
+            
+             ->add("rep_2_$i",TextType::class,[
+                 "label"=> "choix :2"
+             ])
+             ->add("rep_3_$i",TextType::class,[
+                 "label"=> "choix :3"
+             ])
+             ->add("rep_4_$i",TextType::class,[
+                 "label"=> "choix :4"
+             ])
+             ->add("corect_reponse$i", ChoiceType::class, [
+                'choices' => ["choix 1" => "rep_1_$i",
+                              "choix 2" => "rep_2_$i",
+                              "choix 3" => "rep_3_$i",
+                              "choix 4" => "rep_4_$i"
+                             ],
+                'label' => "Quelle est la bonne reponse  ? : "
+            ])
+            ;
+            
+        }
+
+        $view =  $form->getForm()->createView();
+
+        return $this->render('quizz/create.html.twig', [
+            "monform" => $view
+        ]);
+    }
+
+    /**
+     * @Route("/quizz/{id_quizz}", name="quizz_show" )
      */
     public function showQuizz(Request $request, QuestionRepository $questionRepository, ReponseRepository $reponse, $id_quizz, PaginatorInterface $paginator, EntityManagerInterface $em, SessionInterface $session): Response
     {
@@ -53,10 +98,10 @@ class QuizzController extends AbstractController
                 $score[$request->query->getInt('id_question')] = $request->request->get("form", null)["reponse"];
                 $session->set("score", $score);
             }
-            
+
             if (count($session->get('score')) == count($donnees)) {
                 return $this->redirectToRoute('quizz_score', [
-                   // "score" => $score = $session->get('score')
+                    // "score" => $score = $session->get('score')
                 ]);
             }
         }
@@ -107,7 +152,7 @@ class QuizzController extends AbstractController
         $view = $form->createView();
 
         $score = $session->get('score_final', []);
-        $session->set("score_final",[]);
+        $session->set("score_final", []);
 
         return $this->render('quizz/quizz.html.twig', [
             'questions' => $questions,
@@ -119,16 +164,16 @@ class QuizzController extends AbstractController
     /**
      * @Route("/score", name="quizz_score")
      */
-    public function score(SessionInterface $session, Request $request,ReponseRepository $reponseRepository,QuestionRepository $questionRepository)
+    public function score(SessionInterface $session, Request $request, ReponseRepository $reponseRepository, QuestionRepository $questionRepository)
     {
 
 
         $score = $session->get("score");
-        
+
 
         $verifQuestion = [];
         $question_positif = 0;
-        foreach($score as $key => $value){
+        foreach ($score as $key => $value) {
 
             $donnees = $reponseRepository->findBy([
                 "reponse" => $value,
@@ -137,67 +182,52 @@ class QuizzController extends AbstractController
             foreach ($donnees as $value) {
                 if ($value->getIndiceReponse() === 0) {
                     $verifQuestion[$value->getQuestion()->getId()] = 0;
-                
-                }
-                else{
+                } else {
                     $verifQuestion[$value->getQuestion()->getId()] = 1;
                     $question_positif++;
                 }
             }
         }
         $score_pourcentage = $question_positif * count($score);
-       
-        $donnees_final= [];
-        $comp =0;
-        foreach($score as $name_rep){
+
+        $donnees_final = [];
+        $comp = 0;
+        foreach ($score as $name_rep) {
             $comp++;
             $donnees_final[$comp]["repUser"] = $name_rep;
-
         }
         $tab1 = 0;
-        foreach($verifQuestion as $key => $value){
+        foreach ($verifQuestion as $key => $value) {
             $tab1++;
             $donnees_final[$tab1]["result_repUser"] = $value;
-           
 
-            $question = $questionRepository->findBy([
-                  "id" => $key
+
+            $question = $questionRepository->findBy(
+                [
+                    "id" => $key
                 ]
             );
             $donnees_reponse = $reponseRepository->findBy(([
                 "indice_reponse" => 1,
-                "question"=> $key
+                "question" => $key
             ]));
-            foreach($donnees_reponse as $reps){
+            foreach ($donnees_reponse as $reps) {
 
-                $donnees_final[$tab1]["corect"]= $reps->getReponse();
+                $donnees_final[$tab1]["corect"] = $reps->getReponse();
             }
-            
-            foreach($question as $questions){
+
+            foreach ($question as $questions) {
 
                 $donnees_final[$tab1]["question"] = $questions->getQuestion();
                 $donnees_final[$tab1]["index_ques"] = $questions->getIndexQuestion();
-            
             }
-            
         }
-              $session->set('score_final',$donnees_final);
-              $score= $session->get('score_final');
-        
+        $session->set('score_final', $donnees_final);
+        $score = $session->get('score_final');
+
         return $this->render('quizz/resultat.html.twig', [
             "result" => $score,
             "score" => $score_pourcentage,
         ]);
-      
-       
-       
-    }
-
-    /**
-     * @Route("/quizz/create", "quizz_create")
-     */
-    public function create(){
-
-       
     }
 }
