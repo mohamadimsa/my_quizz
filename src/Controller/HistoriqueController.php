90871e744ff5c0714a1d63aad47cd0e13e15ec67
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-05-16 10:14:45 +0200

    historique et corection ok

--- src/Controller/HistoriqueController.php
+++ src/Controller/HistoriqueController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\HistoriqueRepository;
+use App\Repository\ReponsehistoriqueRepository;
+use PhpParser\Node\Stmt\ElseIf_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,32 +14,81 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class HistoriqueController extends AbstractController
 {
     #[Route('/user/{pseudo}/historique', name: 'user_historique')]
-    public function index($pseudo,HistoriqueRepository $historiqueRepository): Response
-    {  
+    public function index($pseudo, HistoriqueRepository $historiqueRepository): Response
+    {
         $user = $this->getUser();
-        
-        if($user->getPseudo() !== $pseudo){
-           
-            return $this->redirectToRoute("user_historique",[
-                "pseudo"=> $user->getPseudo()
+
+        if ($user->getPseudo() !== $pseudo) {
+
+            return $this->redirectToRoute("user_historique", [
+                "pseudo" => $user->getPseudo()
             ]);
         }
 
-        $all_historique = $historiqueRepository->findOneBy([
-            
-            "Users" => $user->getId()
-        ],[
-            "date" => "DESC"
-        ]
-    );
+        $all_historique = $historiqueRepository->findBy(
+            [
 
-    dd($all_historique);
-        
-        
-        
+                "Users" => $user->getId()
+            ],
+            [
+                "date" => "DESC"
+            ]
+        );
+
 
         return $this->render('historique/index.html.twig', [
-            'controller_name' => 'HistoriqueController',
+            'historiques' => $all_historique,
         ]);
+    }
+
+
+    #[Route('/user/{pseudo}/correction/{id}', name: 'user_correction')]
+    public function corection($pseudo, $id, ReponsehistoriqueRepository $reponsehistoriqueRepository, HistoriqueRepository $historiqueRepository): Response
+    {
+        $user = $this->getUser();
+
+
+
+        $verif_his = $historiqueRepository->find($id);
+     
+        if($verif_his !== null){
+            if ($verif_his->getUsers()->getId() !== $user->getId()) {
+                return $this->redirectToRoute("user_historique", [
+                    "pseudo" => $user->getPseudo()
+                ]);
+            }
+        }
+        elseif($verif_his === null){
+            return $this->redirectToRoute("user_historique", [
+                "pseudo" => $user->getPseudo()
+            ]);
+        }
+
+        if ($user->getPseudo() !== $pseudo) {
+
+            return $this->redirectToRoute("user_historique", [
+                "pseudo" => $user->getPseudo()
+            ]);
+        }
+
+        
+
+        $historique = $reponsehistoriqueRepository->findOneby([
+            "historique" => $id
+        ]);
+
+       
+       
+
+        
+
+       
+
+
+
+        return $this->render('quizz/resultat.html.twig', [
+            'result' => $historique->getReponse(),
+            'score' =>  $verif_his->getScore() 
+            ]);
     }
 }
