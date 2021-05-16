f5173626c895ecb866748e7019020a412b5fcd92
Mahamat Abakar <mahamat.abakar-hassan@epitech.eu>
2021-05-16 13:58:06 +0200

    modification des pages

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -37,6 +37,7 @@ class RegistrationFormType extends AbstractType
                 'invalid_message' => 'The password fields must match.',
                 'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
+                
                 'first_options'  => ['label' => 'Password'],
                 'second_options' => ['label' => 'Repeat Password'],
             ])
